717411e535ac728c79f6035ea83782c9d7f9bf33
diasdev15 <utsmand91@gmail.com>
2021-10-24 09:12:00 +0700

    Beatify files

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,15 +2,15 @@ const display = document.getElementById('todoList')
 const create = document.getElementById('todoCreate')
 const btnCreate = document.getElementById('btnSubmit')
 const isDuplicate = (todo) => [...document.querySelectorAll('#todoList li span')].map(e => e.innerText).some(e => e.toLowerCase() == todo.toLowerCase())
-if(!localStorage['maxID'])localStorage['maxID'] = 1
+if (!localStorage['maxID']) localStorage['maxID'] = 1
 let temp = ''
 let editOpen = false
 let canDelete = true
 
 // display item from localStorage
-for(const i of Object.keys(localStorage).sort((a , b) => parseInt(a.substring(4)) - parseInt(b.substring(4)))) {
+for (const i of Object.keys(localStorage).sort((a, b) => parseInt(a.substring(4)) - parseInt(b.substring(4)))) {
     console.log(i)
-    if(i != 'maxID')
+    if (i != 'maxID')
         display.innerHTML += `<li>
         <span>${capitilize(localStorage.getItem(i))}</span>
         <div>
@@ -20,19 +20,37 @@ for(const i of Object.keys(localStorage).sort((a , b) => parseInt(a.substring(4)
         </li>`
 }
 
-function capitilize(s) {return s[0].toUpperCase() + s.substring(1).toLowerCase()}
+function capitilize(s) {
+    return s[0].toUpperCase() + s.substring(1).toLowerCase()
+}
 
 
 const resetButtonStyle = () => {
-    document.querySelectorAll('#btnEdit').forEach(e => {e.style.filter = '';e.style.opacity = '1';e.style.cursor = 'pointer'})
-    document.querySelectorAll('#btnRemove').forEach(e => {e.style.filter = '';e.style.opacity = '1';e.style.cursor = 'pointer'})
+    document.querySelectorAll('#btnEdit').forEach(e => {
+        e.style.filter = '';
+        e.style.opacity = '1';
+        e.style.cursor = 'pointer'
+    })
+    document.querySelectorAll('#btnRemove').forEach(e => {
+        e.style.filter = '';
+        e.style.opacity = '1';
+        e.style.cursor = 'pointer'
+    })
     editOpen = false
     canDelete = true
 }
 
 const disabledButtonStyle = () => {
-    document.querySelectorAll('#btnEdit').forEach(e => {e.style.filter = 'grayscale(500%)';e.style.opacity = '.5';e.style.cursor = 'default'})
-    document.querySelectorAll('#btnRemove').forEach(e => {e.style.filter = 'grayscale(500%)';e.style.opacity = '.5';e.style.cursor = 'default'})
+    document.querySelectorAll('#btnEdit').forEach(e => {
+        e.style.filter = 'grayscale(500%)';
+        e.style.opacity = '.5';
+        e.style.cursor = 'default'
+    })
+    document.querySelectorAll('#btnRemove').forEach(e => {
+        e.style.filter = 'grayscale(500%)';
+        e.style.opacity = '.5';
+        e.style.cursor = 'default'
+    })
     editOpen = true
     canDelete = false
 }
@@ -43,8 +61,8 @@ create.addEventListener('submit', (e) => e.preventDefault())
 
 // if button 'add' is clicked
 btnCreate.addEventListener('click', () => {
-    if(!btnCreate.previousElementSibling.value)alert('Isi dulu bray...')
-    else if (isDuplicate(btnCreate.previousElementSibling.value))alert('To Do has been added')
+    if (!btnCreate.previousElementSibling.value) alert('Isi dulu bray...')
+    else if (isDuplicate(btnCreate.previousElementSibling.value)) alert('To Do has been added')
     else {
         display.innerHTML += `<li>
             <span>${capitilize(btnCreate.previousElementSibling.value)}</span>
@@ -64,13 +82,13 @@ display.addEventListener('click', (e) => {
     //console.log(e.target.id)
 
     //(DELETE BUTTON)
-    if(e.target.id === 'btnRemove' && canDelete){
+    if (e.target.id === 'btnRemove' && canDelete) {
         localStorage.removeItem(e.target.getAttribute('class'))
         e.target.parentElement.parentElement.remove()
-    }   
+    }
 
     //(EDIT BUTTON)
-    else if(e.target.id === 'btnEdit' && !editOpen) {
+    else if (e.target.id === 'btnEdit' && !editOpen) {
         temp = e.target.parentElement.previousElementSibling.innerText
         e.target.parentElement.parentElement.innerHTML = `
             <input type="text" value="${e.target.parentElement.previousElementSibling.innerText}" id="editString" autofocus>
@@ -83,8 +101,8 @@ display.addEventListener('click', (e) => {
     }
 
     // (Save Button)
-    else if(e.target.id === 'btnSave') {
-        if(isDuplicate(e.target.parentElement.previousElementSibling.value))alert('To Do has been added')
+    else if (e.target.id === 'btnSave') {
+        if (isDuplicate(e.target.parentElement.previousElementSibling.value)) alert('To Do has been added')
         else {
             localStorage.setItem(e.target.getAttribute('class'), e.target.parentElement.previousElementSibling.value.toLowerCase())
             e.target.parentElement.parentElement.innerHTML = `
@@ -99,7 +117,7 @@ display.addEventListener('click', (e) => {
     }
 
     // (Cancel button)
-    else if(e.target.id === 'btnCancel') {
+    else if (e.target.id === 'btnCancel') {
         e.target.parentElement.parentElement.innerHTML = `
             <span>${capitilize(temp)}</span>
             <div>
